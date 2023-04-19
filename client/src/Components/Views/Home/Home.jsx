a5c917e022deaae1642227d02feaee4a36edbc1d
GonzaloTuset <soracerito@gmail.com>
2023-04-19 11:26:48 -0300

    muestra todo bien

--- client/src/Components/Views/Home/Home.jsx
+++ client/src/Components/Views/Home/Home.jsx
@@ -1,12 +1,33 @@
+import React from "react";
+import { useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux';
 import NavBar from "../../NavBar/NavBar"
+import { fetchData } from "../../../redux/actions";
 
 const Home = () =>{
+  const dispatch = useDispatch()
+  const selector = useSelector(state=>state.dogs)
+  useEffect(()=>{
+    dispatch(fetchData())
+  },[])
   return(
     <div>
       <header>
        <NavBar/>
         </header>
-    <h1>Home</h1>
+    <div>
+      {
+        selector.map(({id,image,name,temperament,temperaments,weight})=>{
+          return(
+        <div key={id}>
+          <img src={image}/>
+          <h1>{name}</h1>
+          <h2>{temperament}{temperaments}</h2>
+          <h3>{weight}</h3>
+        </div>
+          )})
+      }
+    </div>
     
     </div>
   )
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,3 +1,21 @@
+import {
+  FETCH_DATA_SUCCESS
+} from './actions';
+
 const initialState = {
-  
+  dogs:[]
 };
+
+const rootReducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCH_DATA_SUCCESS:
+    return{
+      ...state,
+      dogs:[...state.dogs, ...action.payload]
+    }
+    default:
+      return { ...state };
+  
+  }
+}
+export default rootReducer
