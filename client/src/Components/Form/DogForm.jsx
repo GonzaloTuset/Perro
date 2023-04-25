51a2f1bd288fafe9fc4e1bc4def905717b534069
GonzaloTuset <soracerito@gmail.com>
2023-04-25 15:45:01 -0300

    regex

--- client/src/Components/Form/DogForm.jsx
+++ client/src/Components/Form/DogForm.jsx
@@ -1,31 +1,51 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 
 const CreateDogForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
-    const currentTarget = event.currentTarget
+    const regex =new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$')
+    const { name, years, image, minHeight, minWeight, maxHeight, maxWeight, temp } = event.currentTarget
     const newDog = {
-      name: currentTarget.name.value,
-      height: currentTarget.minHeight.value + ' - ' + currentTarget.maxHeight.value,
-      weight: currentTarget.minWeight.value + ' - ' + currentTarget.maxWeight.value,
-      years: currentTarget.years.value,
-      temperaments: [currentTarget.temp.value],
-      image: currentTarget.image.value
+      name: name.value,
+      height: minHeight.value + ' - ' + maxHeight.value,
+      weight: minWeight.value + ' - ' + maxWeight.value,
+      years: years.value,
+      temperaments: [temp.value],
+      image: image.value
+    }
+    if(minHeight.value > maxHeight.value){
+      alert('el valor de minHeight no puede ser mayor al de maxHeight')
+      return
+    }
+    if(minWeight.value > maxWeight.value){
+      alert('el valor de minWeight no puede ser mayor al de maxWeight')
+      return
+    }
+    if(!regex.test(image.value)){
+      alert('tiene que ser una URl si o si')
+      return
     }
     axios.post('http://localhost:3001/dogs',newDog)
+    console.log('perro creado')
   }
   
   return(
     <div>
       <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
         <input autoFocus  placeholder='name' type = 'text' name='name'/>
-        <input placeholder='only Url Img, max size: 30'type='url' name='image'/>
-        <input placeholder='min-height' autoComplete='off' type='number' name='minHeight' />
-        <input placeholder='max-height' autoComplete='off' type='number' name='maxHeight' />
-        <input placeholder='min-weight' autoComplete='off' type='number' name='minWeight' />
-        <input placeholder='max-weight' autoComplete='off' type='number' name='maxWeight' />
-        <input placeholder='years' autoComplete='off' type='number' name='years' />
+        <label htmlFor="image"> Image: </label>
+        <input name='image'/>
+        <label htmlFor="Height"> Height: </label>
+        <input placeholder='min-height' type='number' name='minHeight' />-
+        <input placeholder='max-height' type='number' name='maxHeight' />
+        <label htmlFor="name"> Weight: </label>
+        <input placeholder='min-weight' type='number' name='minWeight' />-
+        <input placeholder='max-weight' type='number' name='maxWeight' />
+        <label htmlFor="name"> Life Spam: </label>
+        <input placeholder='life-Spam' type='number' name='years' />
+        <label htmlFor="name"> Temperaments: </label>
         <input placeholder='temperaments' name='temp' />
         <button>crear nueva raza</button>
       </form>
